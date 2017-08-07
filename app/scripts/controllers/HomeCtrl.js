2574f4c7f18d275c6feb440dd2d00c9bb680ed35
Carolyn Walcott <carolynoliveira@Carolyns-MacBook-Air.local>
2017-08-07 00:21:34 -0400

    Checkpoint 4: List Messages - Completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,5 +1,5 @@
 (function() {
-    function HomeCtrl(Room, $uibModal) {
+    function HomeCtrl(Room, Message,$uibModal) {
       this.listRooms = Room.all;
 
       this.open = function() {
@@ -9,10 +9,15 @@
         });
       }
       // when the button on the home screen is clicked, the modal template will be launched
+      // need function to set the current room so it knows what messages to display
+      this.setCurrentRoom = function(room) {
+        this.currentRoom = room;
+        this.messages = Message.getByRoomId(this.currentRoom.$id);
+      }
 
     }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', 'Message', '$uibModal', HomeCtrl]);
 })();
